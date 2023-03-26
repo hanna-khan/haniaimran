f5b6d435ae3b266b2b7c0eef01cf89eda98b5ba9
hanna-khan <hanna.khan205@gmail.com>
2023-03-26 14:22:03 +0500

    deploy

---
+++ src/constants/index.js
@@ -0,0 +1,582 @@
+// import {
+//   mobile,
+//   backend,
+//   creator,
+//   web,
+//   javascript,
+//   typescript,
+//   html,
+//   css,
+//   reactjs,
+//   redux,
+//   tailwind,
+//   nodejs,
+//   mongodb,
+//   git,
+//   figma,
+//   docker,
+//   meta,
+//   starbucks,
+//   tesla,
+//   shopify,
+//   carrent,
+//   jobit,
+//   tripguide,
+//   threejs,
+//   bootstrap,
+//   next,
+//   solidity,
+//   crescent,
+//   ned,
+//   pechs,
+//   // chat,
+//   // ecommerce,
+//   // weather,
+// } from "../assets";
+
+// export const navLinks = [
+//   {
+//     id: "about",
+//     title: "About",
+//   },
+//   {
+//     id: "work",
+//     title: "Education",
+//   },
+//   {
+//     id: "contact",
+//     title: "Contact",
+//   },
+// ];
+
+// const services = [
+//   {
+//     title: "Web Developer",
+//     icon: web,
+//   },
+//   {
+//     title: "React Native Developer",
+//     icon: mobile,
+//   },
+//   {
+//     title: "Blockchain Developer",
+//     icon: backend,
+//   },
+//   {
+//     title: "Metaverse Developer",
+//     icon: creator,
+//   },
+// ];
+
+// const technologies = [
+//   {
+//     name: "HTML 5",
+//     icon: html,
+//   },
+//   {
+//     name: "CSS 3",
+//     icon: css,
+//   },
+//   {
+//     name: "JavaScript",
+//     icon: javascript,
+//   },
+
+//   {
+//     name: "React JS",
+//     icon: reactjs,
+//   },
+//   {
+//     name: "Redux Toolkit",
+//     icon: redux,
+//   },
+//   {
+//     name: "Tailwind CSS",
+//     icon: tailwind,
+//   },
+//   {
+//     name: "Typescript",
+//     icon: typescript,
+//   },
+//   {
+//     name: "Node JS",
+//     icon: nodejs,
+//   },
+//   {
+//     name: "MongoDB",
+//     icon: mongodb,
+//   },
+//   {
+//     name: "Next JS",
+//     icon: next,
+//   },
+//   {
+//     name: "git",
+//     icon: git,
+//   },
+//   {
+//     name: "figma",
+//     icon: figma,
+//   },
+//   {
+//     name: "Solidity",
+//     icon: solidity,
+//   },
+//   {
+//     name: "Bootstrap",
+//     icon: bootstrap,
+//   },
+// ];
+
+// const experiences = [
+//   {
+//     title: "Bachelors",
+//     company_name: "Management Sciences",
+//     icon: ned,
+//     iconBg: "#383E56",
+//     date: "Oct 2022 - September 2026",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Intermediate",
+//     company_name: "PECHS College",
+//     icon: pechs,
+//     iconBg: "#E6DEDD",
+//     date: "August 2020 - August 2022",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   {
+//     title: "Matriculation",
+//     company_name: "The Crescent Academy",
+//     icon: crescent,
+//     iconBg: "#383E56",
+//     date: "April 2018 - April 2020",
+//     points: [
+//       "Developing and maintaining web applications using React.js and other related technologies.",
+//       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//       "Implementing responsive design and ensuring cross-browser compatibility.",
+//       "Participating in code reviews and providing constructive feedback to other developers.",
+//     ],
+//   },
+//   // {
+//   //   title: "Full stack Developer",
+//   //   company_name: "Meta",
+//   //   icon: meta,
+//   //   iconBg: "#E6DEDD",
+//   //   date: "Jan 2023 - Present",
+//   //   points: [
+//   //     "Developing and maintaining web applications using React.js and other related technologies.",
+//   //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+//   //     "Implementing responsive design and ensuring cross-browser compatibility.",
+//   //     "Participating in code reviews and providing constructive feedback to other developers.",
+//   //   ],
+//   // },
+// ];
+// // const experiences = [
+// //   {
+// //     title: "React.js Developer",
+// //     company_name: "Starbucks",
+// //     icon: starbucks,
+// //     iconBg: "#383E56",
+// //     date: "March 2020 - April 2021",
+// //     points: [
+// //       "Developing and maintaining web applications using React.js and other related technologies.",
+// //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+// //       "Implementing responsive design and ensuring cross-browser compatibility.",
+// //       "Participating in code reviews and providing constructive feedback to other developers.",
+// //     ],
+// //   },
+// //   {
+// //     title: "React Native Developer",
+// //     company_name: "Tesla",
+// //     icon: tesla,
+// //     iconBg: "#E6DEDD",
+// //     date: "Jan 2021 - Feb 2022",
+// //     points: [
+// //       "Developing and maintaining web applications using React.js and other related technologies.",
+// //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+// //       "Implementing responsive design and ensuring cross-browser compatibility.",
+// //       "Participating in code reviews and providing constructive feedback to other developers.",
+// //     ],
+// //   },
+// //   {
+// //     title: "Web Developer",
+// //     company_name: "Shopify",
+// //     icon: shopify,
+// //     iconBg: "#383E56",
+// //     date: "Jan 2022 - Jan 2023",
+// //     points: [
+// //       "Developing and maintaining web applications using React.js and other related technologies.",
+// //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+// //       "Implementing responsive design and ensuring cross-browser compatibility.",
+// //       "Participating in code reviews and providing constructive feedback to other developers.",
+// //     ],
+// //   },
+// //   {
+// //     title: "Full stack Developer",
+// //     company_name: "Meta",
+// //     icon: meta,
+// //     iconBg: "#E6DEDD",
+// //     date: "Jan 2023 - Present",
+// //     points: [
+// //       "Developing and maintaining web applications using React.js and other related technologies.",
+// //       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+// //       "Implementing responsive design and ensuring cross-browser compatibility.",
+// //       "Participating in code reviews and providing constructive feedback to other developers.",
+// //     ],
+// //   },
+// // ];
+
+// // const testimonials = [
+// //   {
+// //     testimonial:
+// //       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+// //     name: "Sara Lee",
+// //     designation: "CFO",
+// //     company: "Acme Co",
+// //     image: "https://randomuser.me/api/portraits/women/4.jpg",
+// //   },
+// //   {
+// //     testimonial:
+// //       "I've never met a web developer who truly cares about their clients' success like Rick does.",
+// //     name: "Chris Brown",
+// //     designation: "COO",
+// //     company: "DEF Corp",
+// //     image: "https://randomuser.me/api/portraits/men/5.jpg",
+// //   },
+// //   {
+// //     testimonial:
+// //       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+// //     name: "Lisa Wang",
+// //     designation: "CTO",
+// //     company: "456 Enterprises",
+// //     image: "https://randomuser.me/api/portraits/women/6.jpg",
+// //   },
+// // ];
+
+// const projects = [
+//   {
+//     name: "E-Commerce Store",
+//     description:
+//       "I have developed an ecommerce web application using a combination of cutting-edge technologies, including React.js, Redux, Context API, and Axios. I also implemented dynamic cart functionality that enables seamless navigation and checkout.",
+//     tags: [
+//       {
+//         name: "react",
+//         color: "blue-text-gradient",
+//       },
+//       {
+//         name: "Redux",
+//         color: "green-text-gradient",
+//       },
+//       {
+//         name: "Styled Components",
+//         color: "pink-text-gradient",
+//       },
+//     ],
+//     image: carrent,
+//     source_code_link: "https://thestorecorner.netlify.app/",
+//   },
+//   {
+//     name: "Weather Application",
+//     description:
+//       "Created a React weather app that fetches data from OpenWeatherMap API, uses hooks to manage state and update UI, and converts Unix time to readable format.",
+//     tags: [
+//       {
+//         name: "React",
+//         color: "blue-text-gradient",
+//       },
+//       {
+//         name: "restapi",
+//         color: "green-text-gradient",
+//       },
+//       {
+//         name: "scss",
+//         color: "pink-text-gradient",
+//       },
+//     ],
+//     image: jobit,
+//     source_code_link: "https://weatherproapp-f1e70.web.app/",
+//   },
+//   {
+//     name: "Chat Application",
+//     description:
+//       "Developed a chat application using React and added login functionality with error handling and message sending with image upload feature using Chat Engine API. Rendered chat feed with read receipts.",
+//     tags: [
+//       {
+//         name: "reactjs",
+//         color: "blue-text-gradient",
+//       },
+//       {
+//         name: "api",
+//         color: "green-text-gradient",
+//       },
+//       {
+//         name: "axios",
+//         color: "pink-text-gradient",
+//       },
+//     ],
+//     image: tripguide,
+//     source_code_link: "https://chattyhub.netlify.app/",
+//   },
+// ];
+
+// export { services, technologies, experiences, projects };
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  bootstrap,
+  next,
+  solidity,
+  crescent,
+  ned,
+  pechs,
+  chat,
+  ecom,
+  weather,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Js Developer",
+    icon: mobile,
+  },
+  {
+    title: "Blockchain Developer",
+    icon: backend,
+  },
+  {
+    title: "Metaverse Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Typescript",
+    icon: typescript,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  // {
+  //   name: "Next JS",
+  //   icon: next,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Solidity",
+    icon: solidity,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Starbucks",
+    icon: web,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Staying up-to-date with the latest trends and technologies in front-end development and contributing to the continuous improvement of the development process.",
+      "Writing clean, well-organized, and reusable code using best practices and design patterns, as well as following established coding standards and guidelines.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "Tesla",
+    icon: mobile,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Creating and implementing mobile-specific features such as touch interactions and device-specific functionality.",
+      "Troubleshooting and debugging issues as they arise, both independently and with the team.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Shopify",
+    icon: creator,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Proficiency in React.js and other relevant web technologies such as HTML, CSS, and JavaScript.",
+      "Ability to collaborate effectively with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Participation in code reviews and providing constructive feedback to other developers to ensure high-quality code standards are maintained.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Meta",
+    icon: backend,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Maintaining web applications using React.js and other related technologies. This includes creating new features, fixing bugs, and improving the overall performance of the application.",
+      "Ensure that the web application is responsive and looks great on all devices. This includes testing the application on different screen sizes and making necessary adjustments to ensure that the application is easy to use and navigate.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+    ],
+  },
+];
+
+const projects = [
+  {
+    name: "Ecommerce Store",
+    description:
+      "I have developed an ecommerce web application using a combination of cutting-edge technologies, including React.js, Redux, Context API, and Axios. I also implemented dynamic cart functionality that enables seamless navigation and checkout.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "axios",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ecom,
+    source_code_link: "https://thestorecorner.netlify.app/",
+  },
+  {
+    name: "Weather Application",
+    description:
+      "Created a React weather app that fetches data from OpenWeatherMap API, uses hooks to manage state and update UI, and converts Unix time to readable format. ",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: weather,
+    source_code_link: "https://weatherproapp-f1e70.web.app/",
+  },
+  {
+    name: "Chat Application",
+    description:
+      "Developed a chat application using React and added login functionality with error handling and message sending with image upload feature using Chat Engine API. Rendered chat feed with read receipts.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "api",
+        color: "green-text-gradient",
+      },
+      {
+        name: "styled-components",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: chat,
+    source_code_link: "https://chattyhub.netlify.app/",
+  },
+];
+
+export { services, technologies, experiences, projects };
